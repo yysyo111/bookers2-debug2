54cd84b0b440e2198f6e613142dc3ceb3af8209f
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-05-01 12:50:24 +0000

    [Add]いいね機能追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,14 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  
   validates :title, presence:true
   validates :body, presence:true, length:{maximum:200}
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+  
+  # 引数で渡されたユーザidがFavoritesテーブル内に存在（exists?）するかどうか
+
 end
