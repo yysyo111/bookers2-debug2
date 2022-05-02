d2c6c62434c8ac795f853db0a6b1dec716cbdb8a
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-05-02 07:52:34 +0000

    [Add]応用課題４:機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,29 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  
+  # フォローしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+  
+  # class_name: "Relationship"でRelationshipテーブルを参照,relationshipsとreverse_of_relationshipsは名前の区別、foreign_key(外部キー)、
+  
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
