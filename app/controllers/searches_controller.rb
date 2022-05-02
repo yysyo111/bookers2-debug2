18c0bc6a485a08a54058eed42419637ea3cc3241
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-05-02 14:23:17 +0000

    [Finish]応用課題5終了

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,18 @@
 class SearchesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def search 
+    @range = params[:range]
+    
+    if @range == "User"
+      @users = User.looks(params[:search], params[:word])
+    else
+      @books = Book.looks(params[:search], params[:word])
+    end
+  end
 end
+
+# 検索モデル→params[:range]
+# 検索方法→params[:search]
+# 検索ワード→params[:word]
+# 検索フォームからの情報を受け取る
--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,22 @@ class Book < ApplicationRecord
   validates :title, presence:true
   validates :body, presence:true, length:{maximum:200}
   
+  
+  # 検索方法分岐
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @book = Book.where("title LIKE?","#{word}")
+    elsif search == "forward_match"
+      @book = Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("title LIKE?","%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
+  
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
